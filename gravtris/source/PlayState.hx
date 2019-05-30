42ddd6706ece34cd3383eaa522ee01e6977c4a9d
iamsosmart19 <45020382+iamsosmart19@users.noreply.github.com>
2019-05-30 15:00:36 +1000

    Update PlayState.hx

--- gravtris/source/PlayState.hx
+++ gravtris/source/PlayState.hx
@@ -17,7 +17,7 @@ class PlayState extends FlxState
 		var curx:Int = 0;
 		var stx:Int = curx;
 		var gap:Int = 2;
-		this.sprs = New Array();
+		this.sprs = new Array;
 		//3 by 3 matrix, in case it wasnt obvious
 		for (i in 0...2)
 		{
