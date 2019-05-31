d84ba321334168d54f0e3c18576ae361705dc7e4
e-dt <punycod.edt@gmail.com>
2019-05-31 14:02:34 +1000

    test1

--- gravtris/export/neko/haxe/ApplicationMain.hx
+++ gravtris/export/neko/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "12";
+		app.meta["build"] = "22";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "gravtris";
 		app.meta["name"] = "gravtris";
