8d7ebc0e3b9411f9406d49e94636b7290f31b1fd
e-dt <punycod.edt@gmail.com>
2019-06-03 14:55:04 +1000

    aw hell yea

--- gravtris/source/PlayState.hx
+++ gravtris/source/PlayState.hx
@@ -43,14 +43,33 @@ class PlayState extends FlxState
 
 	override public function update(elapsed:Float):Void
 	{
-
-		for (row in this.tiles)
+		//do something -- anything!
+		var tileIter:Iterator<Array<Int>> = this.tiles.iterator();
+		var sprsIter:Iterator<Array<FlxSprite>> = this.sprs.iterator();
+		var curx:Int = 0;
+		var cury:Int = 0;
+		while (tileIter.hasNext() && sprsIter.hasNext())
 		{
-			for (tile in row)
+			var tileRow:Array<Int>= tileIter.next();
+			var sprsRow:Array<FlxSprite> = sprsIter.next();
+			var trowItr:Iterator<Int> = tileRow.iterator();
+			var srowItr:Iterator<FlxSprite> =sprsRow.iterator();
+			while (trowItr.hasNext() && srowItr.hasNext())
 			{
-			// do something
+				var tile:Int = trowItr.next();				
+				var spr:FlxSprite  = srowItr.next();
+				if (tile == 0)
+				{
+					spr.color = FlxColor.GRAY;
+				} else if (tile == 1)
+				{
+					spr.color = FlxColor.WHITE;
+				}
 			}
+			
+
 		}
+			
 					
 		super.update(elapsed);
 	}
