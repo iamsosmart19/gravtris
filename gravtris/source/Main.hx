332843ba5d9a6ab89fe08f1eb92ebb4d713ae897
e-dt <punycodedt@gmail.com>
2019-09-12 11:56:46 +1000

    laying the groundwork

--- gravtris/source/Main.hx
+++ gravtris/source/Main.hx
@@ -7,6 +7,6 @@ class Main extends Sprite {
 	public function new() {
 		// does this file own anything? Whom knows
 		super();
-		addChild(new FlxGame(0, 0, PlayState));
+		addChild(new FlxGame(0, 0, MenuState));
 	}
 }
