ff3b0ea0b336a4a5f340d82da95d72c1c4569571
iamsosmart19 <iamsosmart19@gmail.com>
2019-09-14 17:29:05 +1000

    Added Menu

---
+++ gravtris/source/HowTo.hx
@@ -0,0 +1,26 @@
+package;
+
+import flixel.FlxState;
+import flixel.FlxSprite;
+import flixel.ui.FlxButton;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.util.FlxColor;
+import flixel.math.FlxPoint;
+import flixel.math.FlxRandom;
+import flixel.FlxG;
+
+class HowToState extends FlxState {
+	public function clickEnd():Void {
+	       FlxG.switchState(new MenuState());
+	}
+	override public function create():Void {
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void {
+		 //
+		 super.update(elapsed);
+	}
+
+}
--- gravtris/source/MenuState.hx
+++ gravtris/source/MenuState.hx
@@ -12,13 +12,51 @@ import flixel.FlxG;
 
 class MenuState extends FlxState {
 	var playButton:FlxButton;
+	var howToPlayButton:FlxButton;
+	var settingsButton:FlxButton;
+	var creditsButton:FlxButton;
+	var title:FlxText;
+
 	public function clickPlay():Void {
 	       FlxG.switchState(new PlayState());
 	}
+
+	public function clickHowPlay():Void {
+	       FlxG.switchState(new HowToState());
+	}
+
+	public function clickSetPlay():Void {
+	       FlxG.switchState(new SettingsState());
+	}
+
+	public function clickCredPlay():Void {
+	       FlxG.switchState(new CreditsState());
+	}
+
 	override public function create():Void {
+		title = new FlxText(FlxG.width / 2 - 175, FlxG.height / 2 - 100, 400, "GRAVTRIS", 64);
+		add(title);
+
+		//Start Game
 		playButton = new FlxButton(0, 0, "Play", clickPlay);
-		playButton.screenCenter();
+		playButton.setPosition(FlxG.width / 2 - 10 - playButton.width, FlxG.height / 2 - 10);
 		add(playButton);
+
+		//Redirects to how to play screen
+		howToPlayButton = new FlxButton(0, 0, "How To Play", clickHowPlay);
+		howToPlayButton.setPosition(FlxG.width / 2 + 10, FlxG.height / 2 - 10);
+		add(howToPlayButton);
+
+		//Start Game
+		settingsButton = new FlxButton(0, 0, "Settings", clickSetPlay);
+		settingsButton.setPosition(FlxG.width / 2 - 10 - playButton.width, FlxG.height / 2 + 10);
+		add(settingsButton);
+
+		//Start Game
+		creditsButton = new FlxButton(0, 0, "Credits", clickCredPlay);
+		creditsButton.setPosition(FlxG.width / 2 + 10, FlxG.height / 2 + 10);
+		add(creditsButton);
+
 		super.create();
 	}
 
